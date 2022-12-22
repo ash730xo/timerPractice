7625ee58fabd81f3f6a25644958e4c7e56899a1f
ash730xo <109447309+ash730xo@users.noreply.github.com>
2022-12-21 21:10:50 -0500

    Add files via upload

--- Time/timer.js
+++ Time/timer.js
@@ -4,6 +4,7 @@
     https://www.w3schools.com/js/js_dates.asp - date
     https://phoenixnap.com/kb/how-to-get-the-current-date-and-time-javascript - time
     https://www.w3schools.com/js/js_timing.asp - timer loop
+    https://stackoverflow.com/questions/56225643/how-to-make-a-pause-play-button-for-timer-on-javascript - pause & play
 */
 
 //getting element by ID form HTML
@@ -12,25 +13,28 @@ const button2 = document.getElementById("stopButton")
 let display = document.getElementById("timerBox")
 
 //onclick event listener
-button1.addEventListener("click", displayDate)
+button1.addEventListener("click", startTimer)
 button2.addEventListener("click", pauseTimer)
 
-//timer 1000 miliseconds = 1 second
-let timer = setInterval(displayDate, 1000)
+//timer outside of the scope so both functions can use it 
+let timer 
 
-//function for event listener. Gets date and time formates it and displays on screen
-function displayDate() {
-   let date = new Date()
-   let current_date = date.getFullYear()+ " / " + (date.getMonth() + 1 ) + " / " + date.getDate()
-   let current_Time = date.toLocaleTimeString()
-   let dateTime = current_date 
-   display.innerHTML = dateTime + " " +current_Time;
+//display date function 
+function displayTime() {
+
+   // gets current time Hours: Minutes: Minilseconds 
+   const d = new Date
+    
+   //displats the time in a string on the HTML webserver
+   display.innerHTML = d.toLocaleTimeString()
 }
 
+// timer function, gets the displayTime and incriments it
+function startTimer() {
+    timer  = setInterval(displayTime, 1000)
+}
+
+//pauses button at the time the button was pressed 
 function pauseTimer(){
-    let date = new Date()
-    let current_date = date.getFullYear()+ " / " + (date.getMonth() + 1 ) + " / " + date.getDate()
-    let current_Time = date.getHours() + " : " + date.getSeconds() + " : " + date.getMilliseconds()
-    let dateTime = current_date 
-    display.innerHTML = dateTime + " " +current_Time;
+    clearInterval(timer)
 }
